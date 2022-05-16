98ccb43d5a2fc8e90d147f45a3f2bc1b07163e23
luchenqun <luchenqun@qq.com>
2022-05-16 15:19:52 +0800

    Update asset tranfer

--- assetTranfer.js
+++ assetTranfer.js
@@ -13,6 +13,8 @@ const cchainProvider = new ethers.providers.JsonRpcProvider(cChainRpcUrl);
 const xrpc = new RPC(xChainUrl);
 const crpc = new RPC(cChainUrl);
 
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
 // X 链资产往 C 链资产转移
 const tranferAssetXChainToCChain = async () => {
   // 下面的地址是c链地址不同形式表示，对应私钥 39cfe0662cdede90094bf079b339e09e316b1cfe02e92d56a4d6d95586378e38
@@ -21,15 +23,16 @@ const tranferAssetXChainToCChain = async () => {
 
   // 确保在转移资产之前，xAddress 的地址对应的私钥已经导入到了x链
   let balances = await xchain.getAllBalances(xAddress);
-  console.log(xAddress + "before transfer balances = ", JSON.stringify(balances));
+  console.log(xAddress + " before transfer balances = ", JSON.stringify(balances));
 
   balances = await cchainProvider.getBalance(to);
-  console.log(to + "before transfer balances = ", balances.toString());
+  console.log(to + " before transfer balances = ", balances.toString());
 
   const xParam = {
     assetID: "AVAX",
     from: [xAddress],
     to: cTo,
+    changeAddr: xAddress,
     amount: 5000000,
     username,
     password,
@@ -44,19 +47,64 @@ const tranferAssetXChainToCChain = async () => {
   };
   console.log("avax.import", await crpc.post("avax.import", cParam));
 
+  await sleep(1000); // 等一下上链
+
   balances = await xchain.getAllBalances(xAddress);
-  console.log(xAddress + "after transfer balances = ", JSON.stringify(balances));
+  console.log(xAddress + " after transfer balances = ", JSON.stringify(balances));
 
   balances = await cchainProvider.getBalance(to);
-  console.log(to + "after transfer balances = ", balances.toString());
+  console.log(to + " after transfer balances = ", balances.toString());
+};
+
+// C 链资产往 X 链资产转移
+const tranferAssetCChainToXChain = async () => {
+  // 下面的地址是c链地址不同形式表示，对应私钥 39cfe0662cdede90094bf079b339e09e316b1cfe02e92d56a4d6d95586378e38
+  const from = "0x666668F2a2E38e93089B6e6A2e37C854bb6dB7de";
+
+  // 确保在转移资产之前，xAddress 的地址对应的私钥已经导入到了x链
+  let balances = await xchain.getAllBalances(xAddress);
+  console.log(xAddress + " before transfer balances = ", JSON.stringify(balances));
+
+  balances = await cchainProvider.getBalance(from);
+  console.log(from + " before transfer balances = ", balances.toString());
+
+  // 这里没有从那个账号转出的参数，所以不一定是从上面的from转出去的，有可能是你username下面的某个账号
+  const cParam = {
+    assetID: "AVAX",
+    to: xAddress,
+    amount: 5000000,
+    username,
+    password,
+  };
+  console.log("avax.export", await crpc.post("avax.export", cParam));
+
+  const xParam = {
+    to: xAddress,
+    sourceChain: "C",
+    username,
+    password,
+  };
+  console.log("avm.import", await xrpc.post("avm.import", xParam));
+
+  await sleep(1000);
+
+  balances = await xchain.getAllBalances(xAddress);
+  console.log(xAddress + " after transfer balances = ", JSON.stringify(balances));
+
+  balances = await cchainProvider.getBalance(from);
+  console.log(from + " after transfer balances = ", balances.toString());
 };
 
 {
   (async () => {
     try {
+      console.log("tranferAssetXChainToCChain")
       await tranferAssetXChainToCChain();
+
+      // console.log("tranferAssetCChainToXChain")
+      // await tranferAssetCChainToXChain();
     } catch (error) {
-      console.error("error:", error)
+      console.error("error:", error);
     }
   })();
 }
--- keystore.js
+++ keystore.js
@@ -8,6 +8,7 @@ const keystore = avalanche.NodeKeys();
 const pchain = avalanche.PChain();
 const xchain = avalanche.XChain();
 const cchain = avalanche.CChain();
+const receivePrivateKey = "PrivateKey-STjN5PYzi8UJyCfbkmdzRzhGmQSneo9BGHRFMjGeZjYH1djUM"; // 演示互相转移avax的私钥接收地址
 
 {
   (async () => {
@@ -20,7 +21,7 @@ const cchain = avalanche.CChain();
     }
     console.log(await xchain.importKey(username, password, privateKey));
     console.log(await cchain.importKey(username, password, privateKey));
-    console.log(await cchain.importKey(username, password, "PrivateKey-STjN5PYzi8UJyCfbkmdzRzhGmQSneo9BGHRFMjGeZjYH1djUM"));
+    console.log(await cchain.importKey(username, password, receivePrivateKey));
     console.log(await pchain.importKey(username, password, privateKey));
   })();
 }
